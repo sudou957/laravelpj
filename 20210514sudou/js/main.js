bdaa7a10f3a0010a99d176ef50d3e9ebf596423d
須藤 <gag3fx@mail.com>
2021-05-14 15:59:07 +0900

    テスト2週目

--- 20210514sudou/js/main.js
+++
@@ -1,7 +0,0 @@
-const target = document.getElementById("menu");
-target.addEventListener('click', () => {
-  const target = document.getElementById("menu");
-  target.classList.toggle('open');
-  const nav = document.getElementById("nav2");
-  nav.classList.toggle('in');
-});
