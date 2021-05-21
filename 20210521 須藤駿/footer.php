b4ae7aad85621d2a97dbc38d41a1001f466f6af6
須藤 <gag3fx@mail.com>
2021-05-21 16:15:21 +0900

    テスト3週目

--- 20210521 須藤駿/footer.php
+++
@@ -1,8 +0,0 @@
-  <!-- フッター欄 -->
-  <footer class="footer">
-    <small>Copyright &copy; <?php echo date('Y'); ?> coachtechサンプルテスト All Rights Reserved.</small>
-  </footer>
-  <?php wp_footer(); ?>
-  </body>
-
-  </html>
