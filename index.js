67aa278d6cb9c8b011a9f62292c1a6850cf3fc37
須藤 <gag3fx@mail.com>
2021-05-28 15:50:36 +0900

    テスト4週目

---
+++ index.js
@@ -0,0 +1,15 @@
+const array = [2, 4, 7, 5, 4, 3, 8];
+
+const indexJS = array.filter(indexOf(item) {
+  return item == 4 ;
+});
+
+console.log(array.indexJS);
+
+const year = 2020;
+
+if (year / 400 == 1) {
+  console.log('うるう年');
+} else if (year / 4 == 1 && year / 100 != 1) {
+  console.log('うるう年');
+}
