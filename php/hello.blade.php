41864a92ebc157c4a42a4ee0c24834a43d7a09f1
須藤 <gag3fx@mail.com>
2021-06-07 11:49:19 +0900

    質問用

---
+++ php/hello.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title')</title>
+  <style>
+    body {
+      font-size: 16pt;
+      margin: 5px;
+    }
+
+    h1 {
+      font-size: 50pt;
+      text-align: right;
+      color: #f6f6f6;
+      margin: -20px 0px -30px 0px;
+      letter-spacing: -4px;
+    }
+
+    .content {
+      margin: 10px;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>@yield('title')</h1>
+  <div class="cotent">
+    @yield('content')
+  </div>
+</body>
+
+</html>
